42485fd047f863bb9aef573024d7bbde875767e7
jaberibnemahboob <ibne0001@stud.kea.dk>
2018-03-26 14:29:59 +0200

    v1.0 - Fixing protocol error of the img src

--- src/js/injectsvg.js
+++ src/js/injectsvg.js
@@ -6,16 +6,25 @@
  */
 document.querySelectorAll('.injectsvg').forEach(function(imgItem){
     let elem;
-    console.log(imgItem);
-    if(imgItem.nodeName.toLowerCase() == 'img'){
-        fetch(imgItem.src).then(function(response){
-            return response.text();
-        }).then(function(svgTxt){
-            elem = document.createElement('div');
-            elem.setAttribute('class',imgItem.classList.value);
-            elem.innerHTML = svgTxt;
-            imgItem.parentNode.insertBefore(elem,imgItem);
-            imgItem.parentNode.removeChild(imgItem);
-        });
+    let imgUrlProtocol="";
+    if(imgItem.nodeName.toLowerCase() == 'img' && imgItem.src != ""){
+        imgUrlProtocol = getUrlProtocol(imgItem.src);
+        if(imgUrlProtocol == "http:" || imgUrlProtocol == "https:"){
+            fetch(imgItem.src).then(function(response){
+                return response.text();
+            }).then(function(svgTxt){
+                elem = document.createElement('div');
+                elem.setAttribute('class',imgItem.classList.value);
+                elem.innerHTML = svgTxt;
+                imgItem.parentNode.insertBefore(elem,imgItem);
+                imgItem.parentNode.removeChild(imgItem);
+            });
+        }
     }else console.log(imgItem.nodeName);
 });
+
+function getUrlProtocol(url){
+    if(url.indexOf("https") === 0) return "https:";
+    else if(url.indexOf("http") === 0) return "http:";
+    else return (window.location.protocol || document.location.protocol);
+}
